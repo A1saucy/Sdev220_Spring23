75d40bcfd426720d0747618f9e38460deb341ba7
A1saucy <90161164+A1saucy@users.noreply.github.com>
2023-02-06 23:53:41 -0500

    Update M03_Lab.py

--- M03_Lab_Casestudy/M03_Lab.py
+++ M03_Lab_Casestudy/M03_Lab.py
@@ -1,39 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  5 07:56:32 2023
-
-@author: mrapp
-"""
-if __name__== "__main__":
-    pass
-
-
-def Vehicle(): 
-    def automobile():
-        year = input("the year: ")
-        make = input("the make: ")
-        model = input("the model: ")
-        doors = input("number of doors: ")
-        roof = input("Sunroof or nothing: ")
-        print( "Your car is a", str(year) , make, model, "and has", str(doors),"doors", "and a", roof + ".")
-    def truck():
-        print("Alright big shot")
-    def plane():
-        print("You are part of the mile high club")
-    def boat():
-        print("That's pretty swell cap")
-    def broomstick():
-        print("You scary")
+class Vehicle():
+    def __init__(self, car, truck, plane, boat, broomstick):
+        self.car = car
+        self.truck = truck
+        self.plane = plane
+        self.boat = boat
+        self.broomstick = broomstick
         
-    words = (input("What do you drive? "))
-    if words == 'car':
-        return automobile()
-    elif words == 'truck':
-        return truck()
-    elif words == 'plane':
-        return plane()
-    elif words == 'boat':
-        return boat()
-    elif words == 'broomstick':
-        return broomstick()
-Vehicle()
+    
+    
+class Automobile():
+    def __init__(self, year, make, model, doors, roof):
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+        
+        
+    def getInfo(self, year, make, model, doors,roof,*args, **kwargs): 
+        year = 2012
+        make = honda
+        model = civic
+        doors = 4
+        roof = sunroof
+        
+    def __repr__(self):
+        return f"Your car was made in {self.year}. Your car is a {self.make} your {self.make} is a {self.model} and it has {self.doors} doors. There is a {self.roof}"   
+     
+class Truck(Vehicle):
+    def __init__(self):
+        return "Alright big shot"
+    
+class Plane(Vehicle):
+    def __init__(self):
+        return "You are part of the mile high club"
+    
+class Boat(Vehicle):
+    def __init__(self):
+        return "That's pretty swell cap"
+    
+class Broomstick(Vehicle):
+    def __init__(self):
+        return "You scary"
+        
+ride = Automobile(2012, "honda", "civic", 4,"sunroof")
+print(ride)
